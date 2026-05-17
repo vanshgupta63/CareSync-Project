ecb99016377e3022423a02408c1c2e71ace9baaa
Your GitHub Name <yourgithubemail@example.com>
2026-05-17 16:06:17 +0530

    added update

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', { autoConnect: false });
+const socket = io('https://caresync-project.onrender.com', { autoConnect: false });
 
 export default socket;
